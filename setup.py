0d9e906c790b583009103971ebb251ddba192ba9
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-05-21 08:16:56 +0100

    adding setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""bsr setup module."""
+import setuptools
+
+
+setuptools.setup(name='bsr',
+                 version='0.0.0',
+                 description=(
+                     'Code for the Bayesian sparse reconstruction paper.'),
+                 url='https://github.com/ejhigson/bsr',
+                 author='Edward Higson',
+                 author_email='e.higson@mrao.cam.ac.uk',
+                 license='MIT',
+                 packages=['bsr'],
+                 install_requires=['numpy>=1.13',
+                                   'scipy>=1.0.0',
+                                   'matplotlib',
+                                   'PIL',
+                                   'nestcheck'])
