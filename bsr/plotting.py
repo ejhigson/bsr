79dcaeaa92b990e1072d1beff0a9ca5140986d35
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-07-17 16:38:53 +0100

    adding latex matplotlib settings to plots and changing nns back to using B={} titles

--- bsr/plotting.py
+++ bsr/plotting.py
@@ -197,7 +197,7 @@ def get_default_titles(likelihood_list, combine, plot_data,
         for nfunc in nfunc_list:
             if isinstance(nfunc, list):
                 # Display only the hidden layers' number of nodes
-                titles.append('${}$'.format(nfunc[1:]))
+                titles.append('$B={}$'.format(nfunc[-1]))
             else:
                 titles.append('$B={}$'.format(nfunc))
     return titles
