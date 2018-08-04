90de79dcdeddc9496ab643a4a7d6bd12d01cd77b
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-08-04 14:32:18 +0100

    starting to separate out results calculations

--- bsr/plotting.py
+++ bsr/plotting.py
@@ -4,6 +4,7 @@ import functools
 import copy
 import warnings
 import numpy as np
+import pandas as pd
 import scipy.special
 import matplotlib
 import matplotlib.gridspec as gridspec
@@ -34,6 +35,63 @@ def adaptive_logz(run, logw=None, nfunc=1):
         return -np.inf
     else:
         return scipy.special.logsumexp(points)
+
+
+def get_bayes_df(problem_data, **kwargs):
+    adfam = kwargs.pop('adfam', False)
+    n_simulate = kwargs.pop('n_simulate', 5)
+    run_list_list = []
+    if adfam:
+        nfunc_list = list(range(1, 11))
+    else:
+        nfunc_list = bsr.results_utils.nfunc_list_union(problem_data)
+    if any(isinstance(nf, list) for nf in nfunc_list):
+        assert all(isinstance(nf, list) for nf in nfunc_list), nfunc_list
+        nfunc_list = [nf[-1] for nf in nfunc_list]
+    df_list = []
+    for meth_key, meth_data in problem_data.items():
+        adaptive, dynamic_goal, _, _ = meth_key
+        run_list = meth_data['run_list']
+        if not adaptive:
+            # vanilla bayes
+            bayes = np.asarray([nestcheck.estimators.logz(run) for run in
+                                run_list])
+            bayes -= bayes.max()
+            # std calculation of different runs parallelised and uses simulated
+            # weights method as it gives the correct results for logZ
+            bayes_stds = nestcheck.parallel_utils.parallel_apply(
+                nestcheck.error_analysis.run_std_simulate, run_list,
+                func_args=([nestcheck.estimators.logz],),
+                func_kwargs={'n_simulate': n_simulate})
+            bayes_stds = np.squeeze(np.asarray(bayes_stds))
+        else:
+            # Adaptive bayes
+            assert len(run_list) == 1
+            if adfam:
+                theta = run_list[0]['theta']
+                # remove T column and add 5 to adaptive column when T=2
+                theta[np.where(theta[:, 0] >= 1.5), 1] += 5
+                theta = theta[:, 1:]
+                run_list[0]['theta'] = theta
+            funcs = [functools.partial(adaptive_logz, nfunc=nf) for nf in
+                     nfunc_list]
+            bayes = nestcheck.ns_run_utils.run_estimators(run_list[0], funcs)
+            bayes -= bayes.max()
+            bayes_stds = nestcheck.error_analysis.run_std_bootstrap(
+                run_list[0], funcs, n_simulate=n_simulate)
+        # Now put into df
+        label = 'adaptive' if adaptive else 'vanilla'
+        if dynamic_goal is not None:
+            label += ' dg={}'.format(dynamic_goal)
+        df_temp = pd.DataFrame(np.vstack([bayes, bayes_stds]),
+                               index=['value', 'uncertainty'], columns=nfunc_list)
+        df_temp.index.name = 'result type'
+        df_temp['label'] = label
+        df_list.append(df_temp)
+    df = pd.concat(df_list)
+    df.set_index('label', append=True, inplace=True)
+    df.reorder_levels(['label', 'result type'])
+    return df
 
 
 def plot_bayes_prob_dict(problem_data, **kwargs):
