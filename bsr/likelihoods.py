9c4a5ad990b3e0e2a2780e797a7f72b1afe9aba9
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-08-19 11:00:41 +0100

    moving to comma seperated strings for data arrays

--- bsr/likelihoods.py
+++ bsr/likelihoods.py
@@ -332,7 +332,8 @@ def cpp_format_array(array):
     file."""
     format_dict = {'\n': '',
                    '[': '',
-                   ']': ''}
+                   ']': '',
+                   ' ': ''}
     # Save input printoptions so we don't edit them
     printoptions_dict_in = np.get_printoptions()
     np.set_printoptions(
@@ -341,12 +342,9 @@ def cpp_format_array(array):
         floatmode='fixed',
         linewidth=10000000)
     vals_str = np.array2string(array.flatten(order='C'), precision=10,
-                               separator=' ')
+                               separator=',')
     for key, item in format_dict.items():
         vals_str = vals_str.replace(key, item)
-    vals_str.replace('  ', ' ')
-    vals_str.replace('  ', ' ')
-    vals_str.replace('  ', ' ')
     assert 'e' not in vals_str, (
         'Should not use scientific notation! vals_str=' + vals_str)
     np.set_printoptions(**printoptions_dict_in)  # restore previous setting
