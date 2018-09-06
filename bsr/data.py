6ffefa4f225b055bc654192c2873b3681a37eb46
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-09-06 18:19:13 +0100

    typo fix and tidying

--- bsr/data.py
+++ bsr/data.py
@@ -123,11 +123,11 @@ def get_data_name(data_func, data_type, npoints, y_error_sigma, x_error_sigma):
 
 
 def get_true_value(data_func, data_type, x1=0.5, x2=None):
-    """Get the noise-free true value for some x coordinate."""
+    """Get the noise-free true value for some coordinate (x1, x2)."""
     if x2 is None:
         x2 = x1  # Not used if func is 1d
-    data = generate_data(data_func, data_type, 0, x1min=x,
-                         x1max=x, x2min=x, x2max=x, npoints=1)
+    data = generate_data(data_func, data_type, 0, x1min=x1,
+                         x1max=x1, x2min=x2, x2max=x2, npoints=1)
     true = data['y_no_noise']
     if true.ndim == 2:
         return true[0, 0]
@@ -167,22 +167,22 @@ def get_data_args(data_func, nfuncs):
         # [a_1, mu1_1, mu2_1, s1_1, s2_1, b1_1, b2_1, rot angle]
         if nfuncs == 1:
             data_args = [
-                {'a': 0.8, 'mu1': 0.6, 'mu2': 0.6, 'sigma1': 0.1, 'sigma2': 0.2,
-                 'beta1': 2, 'beta2': 2, 'omega': 0.1 * np.pi}]
+                {'a': 0.8, 'mu1': 0.6, 'mu2': 0.6, 'sigma1': 0.1,
+                 'sigma2': 0.2, 'beta1': 2, 'beta2': 2, 'omega': 0.1 * np.pi}]
         elif nfuncs == 2:
             data_args = [
-                {'a': 0.5, 'mu1': 0.5, 'mu2': 0.4, 'sigma1': 0.4, 'sigma2': 0.2,
-                 'beta1': 2, 'beta2': 2, 'omega': 0},
-                {'a': 0.8, 'mu1': 0.5, 'mu2': 0.6, 'sigma1': 0.1, 'sigma2': 0.1,
-                 'beta1': 2, 'beta2': 2, 'omega': 0}]
+                {'a': 0.5, 'mu1': 0.5, 'mu2': 0.4, 'sigma1': 0.4,
+                 'sigma2': 0.2, 'beta1': 2, 'beta2': 2, 'omega': 0},
+                {'a': 0.8, 'mu1': 0.5, 'mu2': 0.6, 'sigma1': 0.1,
+                 'sigma2': 0.1, 'beta1': 2, 'beta2': 2, 'omega': 0}]
         elif nfuncs == 3:
             data_args = [
-                {'a': 0.5, 'mu1': 0.3, 'mu2': 0.7, 'sigma1': 0.2, 'sigma2': 0.2,
-                 'beta1': 2, 'beta2': 2, 'omega': 0},
-                {'a': 0.7, 'mu1': 0.7, 'mu2': 0.6, 'sigma1': 0.15, 'sigma2': 0.15,
-                 'beta1': 2, 'beta2': 2, 'omega': 0},
-                {'a': 0.9, 'mu1': 0.4, 'mu2': 0.3,'sigma1': 0.1, 'sigma2': 0.1,
-                 'beta1': 2, 'beta2': 2, 'omega': 0}]
+                {'a': 0.5, 'mu1': 0.3, 'mu2': 0.7, 'sigma1': 0.2,
+                 'sigma2': 0.2, 'beta1': 2, 'beta2': 2, 'omega': 0},
+                {'a': 0.7, 'mu1': 0.7, 'mu2': 0.6, 'sigma1': 0.15,
+                 'sigma2': 0.15, 'beta1': 2, 'beta2': 2, 'omega': 0},
+                {'a': 0.9, 'mu1': 0.4, 'mu2': 0.3, 'sigma1': 0.1,
+                 'sigma2': 0.1, 'beta1': 2, 'beta2': 2, 'omega': 0}]
     try:
         data_args_list = []
         for name in bf.get_bf_param_names(data_func):
